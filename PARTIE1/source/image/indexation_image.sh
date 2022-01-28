3497f2cb4a967140e6d29175df7b7626ac206263
dosrt <93156263+dosrt@users.noreply.github.com>
2022-01-28 17:25:14 +0100

    Update indexation_image.sh

--- PARTIE1/source/image/indexation_image.sh
+++ PARTIE1/source/image/indexation_image.sh
@@ -1,15 +1,10 @@
-# #!/bin/sh
-# echo $(pwd)
-# ls ../../documents/image/TEST_IMAGE/ |grep -v .[jpg]>../../documents/image/liste_noms_images_RGB/temp.txt
-# sed '$d' ../../documents/image/liste_noms_images_RGB/temp.txt > ../../documents/image/liste_noms_images_RGB/liste_noms_images.txt
-# input="../../documents/image/liste_noms_images_RGB/liste_noms_images.txt"
-# gcc indexation_image.c -o indexation_image.out
-# rm ../../documents/image/descripteurs/base_descripteur_image.txt
-# while IFS= read -r line;
-# do
-#     ./indexation_image.out $line
-# done<"../../documents/image/liste_noms_images_RGB/liste_noms_images.txt"
-# rm ../../documents/image/liste_noms_images_RGB/temp.txt
+/*  AUTEUR :    SAURAT DORIAN                                                                               */
+/*                                                                                                          */
+/*  NOM :   indexation_image.sh                                                                             */
+/*                                                                                                          */
+/*  Fonction :  Ce script a pour but de réaliser l'indexation de la totalité des fichiers texte du dossier  */
+/*              TEST_IMAGE par l'appel successif pour chacun des noms de fichiers de l'axécutable de        */
+/*              l'indexation                                                                                */
 
 
 #!/bin/sh
